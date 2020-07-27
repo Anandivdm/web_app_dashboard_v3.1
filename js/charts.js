d500b12ee188ecd1c19fa74ec3fd4a0d2bf7dc44
Anandi Ellis <anandivdm@gmail.com>
2020-07-27 09:24:41 +0200

    bar and doughnut charts

--- js/charts.js
+++ js/charts.js
@@ -1,6 +1,11 @@
 let trafficChart =  document.getElementById('traffic-chart');
+let dailyTrafficChart = document.getElementById('daily-traffic-chart');
+let mobileUsersChart = document.getElementById('mobile-users-chart');
 
-var myChart = new Chart(trafficChart, {
+
+// Traffic
+
+let traffic = new Chart(trafficChart, {
   type: 'line',
   data: {
       labels: ['16-22', '23-29', '30-5', '16-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
@@ -36,3 +41,60 @@ var myChart = new Chart(trafficChart, {
     }
   },
 });
+
+// Daily Traffic
+
+let dailyTraffic = new Chart(dailyTrafficChart, {
+  type: 'bar',
+  data: {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+      label: '# of Hits',
+      data: [75, 115, 175, 125, 225, 200, 100],
+      backgroundColor: '#7477BF',
+      borderWidth: 1,
+      maxBarThickness: 20,
+      }]
+  },
+  options: {
+    responsive: true,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero:true
+        }
+      }]
+    },
+    legend : {
+      display: false
+    }
+  },
+});
+
+// Mobile Users
+
+let mobileUsers = new Chart(mobileUsersChart, {
+  type: 'doughnut',
+  data: {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+      label: '# of Users',
+      data: [2000, 550, 500],
+      borderWidth: 0,
+      backgroundColor: [
+        '#7477BF',
+        '#78CF82',
+        '#51B6C8'
+      ]
+    }]
+  },
+  options: {
+    legend: {
+      position: 'right',
+      labels: {
+        boxWidth: 20,
+        fontStyle: 'bold'
+      }
+    }
+  }
+});x
