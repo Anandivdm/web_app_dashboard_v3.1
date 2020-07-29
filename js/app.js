625c0621bdef280a74bb12aba1e22cad3b75d2c2
Anandi Ellis <anandivdm@gmail.com>
2020-07-29 21:22:37 +0200

    final tweaks

--- js/app.js
+++ js/app.js
@@ -1,13 +1,28 @@
-let alert = document.getElementById('alert');
+let alertMessage = document.getElementById('alert');
+let user = document.getElementById('user');
+let message = document.getElementById('message');
+let send = document.getElementById('send');
 
-alert.innerHTML =
+alertMessage.innerHTML =
 `<div>
   <p><strong>Alert!</strong> How an alert message in this box</p>
   <p class="close-button">x</p>
 </div>`
 
-alert.addEventListener('click', (e) => {
+alertMessage.addEventListener('click', (e) => {
   if(e.target.classList.contains("close-button")) {
     alert.style.display = "none";
   }
-})
+});
+
+send.addEventListener('click', () => {
+  if(user.value === "" && message.value === "") {
+    alert("Please select a user and enter a message before sending");
+  } else if (user.value === "") {
+    alert("Please select a user before sending");
+  } else if (message.value === "") {
+    alert("Please add a message before sending");
+  } else {
+    alert("Your message has been sent!");
+  }
+});
